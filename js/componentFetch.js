e13c5da61252183d593b8b746f455aaa89096537
Line-WH <linerooed.gmail.com>
2025-05-13 11:56:02 +0200

    refactoring af js fetch

---
+++ js/componentFetch.js
@@ -0,0 +1,7 @@
+fetch('navmenu.html')
+    .then(res => res.text())
+    .then(html => document.getElementById('nav-placeholder').innerHTML = html);
+
+fetch('footer.html')
+    .then(res => res.text())
+    .then(html => document.getElementById('footer-placeholder').innerHTML = html);
